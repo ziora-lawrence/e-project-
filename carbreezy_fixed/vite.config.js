d01797dfe9dd33a96ea8ad37594f91fd8bff37be
ziora-lawrence <danieliwuji28@gmail.com>
2026-04-05 02:07:30 +0100

    icl

--- carbreezy_fixed/vite.config.js
+++ carbreezy_fixed/vite.config.js
@@ -3,6 +3,25 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // This base setting is important for Vercel deployment
   base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: false,
+  },
 })
